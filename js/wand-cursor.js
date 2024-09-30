9b1286546f1a06b478915185c09f1df1652afa5b
Scott Kuang <scottkuang05@gmail.com>
2024-09-30 01:40:15 -0400

    Cursor now has a wand following it that perpetuallly shoots out pellets. I would want it to only shoot pellets when I click.

---
+++ js/wand-cursor.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wandCursor = document.getElementById("wand-cursor");
+    const sparkles = document.querySelector(".sparkles");
+
+    if (wandCursor && sparkles) {
+        document.addEventListener("mouseenter", () => { 
+            wandCursor.style.display = 'block'; 
+        });
+
+        document.addEventListener("mouseleave", () => { 
+            wandCursor.style.display = 'none'; 
+        });
+
+        document.addEventListener("mousemove", followCursor);
+
+        document.addEventListener("click", () => {
+            sparkles.classList.add("active");
+            setTimeout(() => {
+                sparklesContainer.classList.remove("active");
+            }, 3000);
+        });
+
+        /* sparkles.addEventListener("transitionend", () => {
+            sparkles.children.forEach( (item, index) => item.classList.remove("active") );
+        }); */
+
+        function followCursor(event) {
+            const wandWidth = wandCursor.clientWidth;
+            const wandHeight = wandCursor.clientHeight;
+            const x = event.clientX + 19 - (wandWidth/2);
+            const y = event.clientY + 22 - (wandHeight/2);
+            // console.log(`Mouse position: (${event.clientX}, ${event.clientY}), Wand position: (${x}, ${y})`);
+            wandCursor.style.transform = `translate(${x}px, ${y}px) rotate(-40deg)`;
+        }
+    } else {
+        console.error("wand-cursor element not found");
+    }
+});
+
